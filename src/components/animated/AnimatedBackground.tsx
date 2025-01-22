03a5dcbe0b241b931497ff157ebb83499f6ee34f
Kushhhhhhhh <kush.73888@gmail.com>
2025-01-22 07:26:55 +0530

    Updated Animated Background

--- src/components/animated/AnimatedBackground.tsx
+++ src/components/animated/AnimatedBackground.tsx
@@ -26,8 +26,8 @@ const AnimatedBackground = () => {
       color: string
 
       constructor() {
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
+        this.x = Math.random() * canvas!.width
+        this.y = Math.random() * canvas!.height
         this.size = Math.random() * 5 + 1
         this.speedX = Math.random() * 3 - 1.5
         this.speedY = Math.random() * 3 - 1.5
@@ -40,15 +40,15 @@ const AnimatedBackground = () => {
 
         if (this.size > 0.2) this.size -= 0.1
 
-        if (this.x < 0 || this.x > canvas.width) this.speedX *= -1
-        if (this.y < 0 || this.y > canvas.height) this.speedY *= -1
+        if (this.x < 0 || this.x > canvas!.width) this.speedX *= -1
+        if (this.y < 0 || this.y > canvas!.height) this.speedY *= -1
       }
 
       draw() {
-        ctx.fillStyle = this.color
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
-        ctx.fill()
+        ctx!.fillStyle = this.color
+        ctx!.beginPath()
+        ctx!.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+        ctx!.fill()
       }
     }
 
